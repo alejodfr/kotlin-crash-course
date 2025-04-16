c3bb7dfe58254d4b1a8fd34a9b8448f1f59768d5
Alejo <alejodfr@gmail.com>
2025-04-16 16:32:25 -0500

    comparison operators

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,9 @@
 fun main(){
     val x = 12
     val y = 11
-    val areBothEven = x % 2 == 0 || y % 2 == 0 //creando una variable para evaluar 2 condiciones
+    val z = 3 + 3 * 3
+    val areBothEven = x % 2 == 0 || y % 2 == 0 && x + y == 25 //creando una variable para evaluar 2 condiciones
+    println(z)
     println("Is x an even number? $areBothEven")
 
 }
