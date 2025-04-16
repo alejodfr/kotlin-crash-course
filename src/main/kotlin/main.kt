d5fa9050e3c82de9088e347a9f429474cbb53b3e
Alejo <alejodfr@gmail.com>
2025-04-16 17:01:10 -0500

    Usando la expresion ?: numero a lado de variable.toIntOrNull() se agrega un comentario explicando

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main(){
     println("Please enter a number:")
     val input = readln()
-    val inputAsInteger = input.toIntOrNull() ?: 0
+    val inputAsInteger = input.toIntOrNull() ?: 0 // esta expresion remplaza el valor que da el usuario en caso de ser null por el numero que en esta ocasion es zero, partiendo del hecho de se introduzca un string que sera traducio en el programa como un null que dara cero
     val isEven = inputAsInteger % 2 == 0
     println("is even: $isEven")
 }
