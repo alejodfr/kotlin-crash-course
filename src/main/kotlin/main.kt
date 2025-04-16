0fc54d318658c828eb0705b81ea5bea01896b87f
Alejo <alejodfr@gmail.com>
2025-04-16 16:57:52 -0500

    Usando la expresion ?: numero a lado de variable.toIntOrNull()

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main(){
     println("Please enter a number:")
     val input = readln()
-    val inputAsInteger = input.toInt() //introduce cualquier dato y transformalo a entero
+    val inputAsInteger = input.toIntOrNull() ?: 0
     val isEven = inputAsInteger % 2 == 0
-    println("Is the number even? $isEven")
+    println("is even: $isEven")
 }
