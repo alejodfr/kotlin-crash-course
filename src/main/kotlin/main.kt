e695fd61b356a11457276200ea6b7d334a730353
Alejo <alejodfr@gmail.com>
2025-04-16 16:26:07 -0500

    comparison operators 5 (true)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 fun main(){
-    val x = 11
-    val y = 9
+    val x = 12
+    val y = 11
     println("Is x an even number? ${x % 2 == 0}") //el operador $ se usa para interpolar variables dentro de cadenas de variables (string)
 
 }
