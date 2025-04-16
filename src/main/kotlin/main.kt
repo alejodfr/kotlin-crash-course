4d3f3f192211fae257a5f8ddcca41f087a3b2424
Alejo <alejodfr@gmail.com>
2025-04-16 16:15:42 -0500

    comparison operators 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main(){
     val x = 11
     val y = 9
-    println(x == y) // esta comparacion se transforma en un boleano
+    println(x > y) // esta comparacion se transforma en un boleano (devuelve el valor true)
 }
