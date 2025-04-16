2c24d7f1a4a63b8672603d8afaba8c2fb8a5c13e
Alejo <alejodfr@gmail.com>
2025-04-16 16:38:45 -0500

    Introduccion de informacion preguntando al usuario

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,5 @@
 fun main(){
-    val x = 12
-    val y = 11
-    val z = 3 + 3 * 3
-    val areBothEven = x % 2 == 0 || y % 2 == 0 && x + y == 25 //creando una variable para evaluar 2 condiciones
-    println(z)
-    println("Is x an even number? $areBothEven")
-
+    println("Please enter a number:")
+    val input = readln()
+    println("You entered a $input")
 }
