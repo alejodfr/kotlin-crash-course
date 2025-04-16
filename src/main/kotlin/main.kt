2ac04ae26e8374c00ede0e2dc0bf43a7b434afa9
Alejo <alejodfr@gmail.com>
2025-04-16 17:23:19 -0500

    usando condicionales

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,9 @@
 fun main(){
     println("Please enter a number:")
     val input = readln()
-    val inputAsInteger = input.toIntOrNull() ?: 0 // esta expresion remplaza el valor que da el usuario en caso de ser null por el numero que en esta ocasion es zero, partiendo del hecho de se introduzca un string que sera traducio en el programa como un null que dara cero
-    val isEven = inputAsInteger % 2 == 0
-    println("is even: $isEven")
+    val inputAsInteger = input.toIntOrNull()
+    if (inputAsInteger != null) {
+        val isEven = inputAsInteger % 2 == 0
+        println("is even: $isEven")
+    }
 }
