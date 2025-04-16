3d1feb7d6f05d0f6e96f7f2e158e9b73dac3b236
Alejo <alejodfr@gmail.com>
2025-04-16 13:38:08 -0500

    some aritmetics operations pt1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,5 @@
 fun main(){
-    val x = 5
-    val y = 5.12 // here we have a float variable being implicit initialized
-    val z: Float = 5f // here we have a float explicit initialized
-    println(x)
-    println(y)
-    println(z)
+    val x = 10
+    val y = 9
+    println(x/y)
 }
