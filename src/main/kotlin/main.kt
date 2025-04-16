bced52ae9faf19e787c79b41935f886733c74aaa
Alejo <alejodfr@gmail.com>
2025-04-16 12:55:43 -0500

    float variables

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,8 @@
 fun main(){
     val x = 5
+    val y = 5.12 // here we have a float variable being implicit initialized
+    val z: Float = 5f // here we have a float explicit initialized
     println(x)
+    println(y)
+    println(z)
 }
