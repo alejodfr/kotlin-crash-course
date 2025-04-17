87a15cf39a96a14db683495a7c6ae17c6512e0f4
Alejo <alejodfr@gmail.com>
2025-04-17 09:58:48 -0500

    Interface
    sumando area de rectangulo 1 y circulo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,17 +12,31 @@ fun main(){
     println(rect2)
 
     println(rect1==rect2)
+
+    println(sumAreas(rect1, circle))
 }
 
+fun sumAreas(vararg shapes: Shape): Double{
+    return shapes.sumOf { curentShape ->
+        curentShape.area.toDouble()
+    }
+}
 
+interface Shape{
+    val area: Float
+    val circumference: Float
+}
 
-data class Rectangle (val width: Float, val height: Float) {
+data class Rectangle (val width: Float, val height: Float): Shape{
 
     val diagonal = sqrt(width * width + height * height)
-    val area = width*height
+    override val area = width*height
+    override val circumference = 2 * width + 2 * height
+
 }
 
-data class Circle(val radius: Float){
-    val area = radius * radius * PI
+data class Circle(val radius: Float): Shape{
+    override val area = radius * radius * PI.toFloat()
+    override val circumference = 2 * radius * PI.toFloat()
     val diameter = 2 * radius
 }
