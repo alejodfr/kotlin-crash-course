e4b82837f43a1324616ffec88c960514d6284985
Alejo <alejodfr@gmail.com>
2025-04-17 00:33:04 -0500

    Loops
    For Loop, imprimiendo cada caracter de un string (recordar que un string es un array de caracteres)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,8 @@
 fun main(){
-    println("how many numbers you will enter?")
-    val amountOfNumbers = readln().toIntOrNull() ?: 0
+    println("Enter a string:")
+    val input = readln()
 
-
-    val numbers = mutableListOf<Int>()
-    for (i in 0 until amountOfNumbers){
-        println("Please enter number #${i + 1}")
-        val number = readln().toIntOrNull() ?: continue
-        numbers.add(number)
-    }
-
-    for(number in numbers) {
-        println("Numbers: $number")
+    for (character in input){
+        println(character)
     }
 }
