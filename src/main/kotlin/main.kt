860c16353306570f8fca4ceee763facb758370a6
Alejo <alejodfr@gmail.com>
2025-04-17 09:21:43 -0500

    Classes data class

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,8 +4,13 @@ import kotlin.math.sqrt
 fun main(){
     val rect1 = Rectangle( width = 5f, height = 7f)
     val rect2 = Rectangle(width = 5f, height = 7f)
+    val rect3 = Rectangle(width = 5f, height = 8f)
+
+    println(rect1)
+    println(rect3)
 
     println(rect1==rect2)
+    println(rect1==rect3)
 }
 
 
