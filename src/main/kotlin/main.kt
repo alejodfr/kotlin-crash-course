bc3286a72424de9fff9b1efec2f69b3546d81cae
Alejo <alejodfr@gmail.com>
2025-04-17 00:38:56 -0500

    Loops
    For Loop, revertir un string hello->olleh

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main(){
     println("Enter a string:")
     val input = readln()
-
+    // reverse string
     val finalString = buildString {
         for (i in input.lastIndex downTo 0){
             append(input[i])
