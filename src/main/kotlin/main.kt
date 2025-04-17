40535b6405c935be66213b0d094b18d2beb6f660
Alejo <alejodfr@gmail.com>
2025-04-17 09:36:47 -0500

    Classes
    data class

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,13 @@
+import kotlin.math.PI
 import kotlin.math.sqrt
 
 
 fun main(){
     val rect1 = Rectangle( width = 5f, height = 7f)
     val rect2 = rect1.copy(height = 10f) // usar la funcion .copy
+    val circle = Circle(radius = 5f)
+
+    println("La area del circulo es ${circle.area}")
 
     println(rect2)
 
@@ -16,4 +20,9 @@ data class Rectangle (val width: Float, val height: Float) {
 
     val diagonal = sqrt(width * width + height * height)
     val area = width*height
+}
+
+data class Circle(val radius: Float){
+    val area = radius * radius * PI
+    val diameter = 2 * radius
 }
