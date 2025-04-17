41b786d2c4d6a926475a60efa15b3a526e1bd7fb
Alejo <alejodfr@gmail.com>
2025-04-16 20:29:16 -0500

    Exceptions, throw

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,18 @@
 fun main(){
     println("Please enter a number:")
     val input = readln()
-    val inputAsInteger = input.toIntOrNull()
+    throw Exception("dont know what an integer is?") // para meter avisos de erros en la consola
+    val inputAsInteger = try {
+        input.toInt()
+    } catch (e: NumberFormatException){
+        0
+    }
 
         val output = when(inputAsInteger) {
-            null -> "enter a valid number"
             3 -> "the number is three!"
             5 -> "the number five"
             in 4..20 -> "the number is between 4 and 20"
-            else -> "No idea"
+            else -> "No idea what to print"
         }
         println(output)
 }
