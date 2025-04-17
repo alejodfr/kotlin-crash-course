36fc00eced1c2ccae7970d0a8f7f86d2b639a646
Alejo <alejodfr@gmail.com>
2025-04-17 09:19:28 -0500

    Classes data class

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,12 +3,15 @@ import kotlin.math.sqrt
 
 fun main(){
     val rect1 = Rectangle( width = 5f, height = 7f)
-    println(rect1.width)
-    println(rect1.height)
-    println("The diagonal is ${rect1.diagonal}")
+    val rect2 = Rectangle(width = 5f, height = 7f)
+
+    println(rect1==rect2)
 }
 
-class Rectangle (val width: Float, val height: Float) {
+
+
+data class Rectangle (val width: Float, val height: Float) {
 
     val diagonal = sqrt(width * width + height * height)
+    val area = width*height
 }
