e58c20c4a49eef9fb57e6f9b7a59d7e2fb8465e7
Alejo <alejodfr@gmail.com>
2025-04-17 00:59:01 -0500

    Functions
    sino pasamos ningun parametro ejemplo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,15 +2,15 @@ fun main(){
     println("Enter a string:")
     val input = readln()
 
-    val reversed = reversed(input)
+    val reversed = reversed()
     println(reversed)
 
-    if(input == reversed(input)){
+    if(input == reversed){
         println("Thats a palindrome!")
     }
 }
 
-fun reversed(stringToReverse: String): String{
+fun reversed(stringToReverse: String = "hello world"): String{
     val finalString = buildString {
         for (i in stringToReverse.lastIndex downTo 0){
             append(stringToReverse[i])
