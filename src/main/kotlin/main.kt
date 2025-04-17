6fe290253bbac94aaf3ff26b78990613a77e0cb0
Alejo <alejodfr@gmail.com>
2025-04-16 20:31:52 -0500

    Arrays

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,4 @@
 fun main(){
-    println("Please enter a number:")
-    val input = readln()
-    throw Exception("dont know what an integer is?") // para meter avisos de erros en la consola
-    val inputAsInteger = try {
-        input.toInt()
-    } catch (e: NumberFormatException){
-        0
-    }
-
-        val output = when(inputAsInteger) {
-            3 -> "the number is three!"
-            5 -> "the number five"
-            in 4..20 -> "the number is between 4 and 20"
-            else -> "No idea what to print"
-        }
-        println(output)
+    val favoriteNumbers = intArrayOf(1, 2, 3, 69)
+    println(favoriteNumbers[1])
 }
