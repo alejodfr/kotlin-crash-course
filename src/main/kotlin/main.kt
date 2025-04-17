174790227a7ddb4f51c7ba6d27cae2ddd4760326
Alejo <alejodfr@gmail.com>
2025-04-17 09:12:23 -0500

    Classes creando clase rectangulo y calculando su diagonal

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,14 @@
+import kotlin.math.sqrt
 
 
 fun main(){
-    val rect1 = Rectangle( width = 5, height = 7)
+    val rect1 = Rectangle( width = 5f, height = 7f)
     println(rect1.width)
-    print(rect1.height)
+    println(rect1.height)
+    println("The diagonal is ${rect1.diagonal}")
 }
 
-class Rectangle (val width: Int, val height: Int) // the constructor odf the class
+class Rectangle (val width: Float, val height: Float) {
+
+    val diagonal = sqrt(width * width + height * height)
+}
