e497b718ac26b06ce7b3801e30511a1162f3a968
Alejo <alejodfr@gmail.com>
2025-04-16 20:39:13 -0500

    Arrays

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,4 @@
 fun main(){
     val favoriteNumbers = intArrayOf(1, 2, 3, 69)
-    println(favoriteNumbers[1])
+    println(favoriteNumbers.getOrNull(3)) // otra forma de obtener los numeros, si pones un numero mayor al tamanio del arreglo dara null
 }
