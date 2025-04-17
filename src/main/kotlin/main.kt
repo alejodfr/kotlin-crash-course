4d545fad9954162cf81dbbfafa01766d80384933
Alejo <alejodfr@gmail.com>
2025-04-17 00:45:40 -0500

    Functions
    (transformando el bloque de codigo de revertir el string en una funcion)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,15 @@
 fun main(){
     println("Enter a string:")
     val input = readln()
-    // reverse string
+
+    reversed(input)
+
+}
+
+fun reversed(stringToReverse: String){
     val finalString = buildString {
-        for (i in input.lastIndex downTo 0){
-            append(input[i])
+        for (i in stringToReverse.lastIndex downTo 0){
+            append(stringToReverse[i])
         }
     }
     println(finalString)
